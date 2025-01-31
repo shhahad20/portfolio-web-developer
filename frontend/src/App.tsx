63e8fcf0ae4f5309c32da9fa48fded31bea0a1d0
shhahad20 <Shhahad20@gmail.com>
2025-01-31 14:58:18 -0800

    feat: add Contact & Footer components with responsive design
    -Created the Contact component and designed it based on the provided reference.
    - Ensured the Contact & Footer section is fully responsive across different screen sizes.
    - Showing a small tooltip-like message above the email div when the user clicks on it.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import ThreeD from "./components/3D";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -13,6 +15,8 @@ function App() {
       <Skills/>
       <ThreeD/>
       <Projects/>
+      <Contact/>
+      <Footer/>
     </>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import "../styles/contact.scss";
+
+const Footer: React.FC = () => {
+  return <footer>
+
+    <h3>Developed by www.shahadaltharwa.com</h3>
+  </footer>;
+};
+
+export default Footer;
